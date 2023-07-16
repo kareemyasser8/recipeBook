3b6d3f6da8a020e3acb20539ca9fc4ba571c04ee
kareemyasser8 <kareem.yasserr@gmail.com>
2023-07-16 11:06:31 +0200

    adding spinner and error alert

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -9,16 +10,38 @@ import { NgForm } from '@angular/forms';
 export class AuthComponent implements OnInit {
 
   isLoginMode = true;
+  isLoading = false;
+  error: string = null;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   onSwitchMode(){
     this.isLoginMode = !this.isLoginMode;
   }
 
   onSubmit(form: NgForm){
-    console.log(form.value);
-    form.reset();
+    if(!form.valid) return;
+    const email = form.value.email;
+    const password = form.value.password;
+
+    this.isLoading = true;
+    if(this.isLoginMode){
+
+    }else{
+      this.authService.signup(email,password).subscribe({
+        next: (repsonse)=>{
+          console.log(repsonse);
+          this.isLoading = false;
+        },
+        error: (err)=> {
+          console.log(err);
+          this.error = 'An Error occured!'
+          this.isLoading = false;
+        }
+      });
+      form.reset();
+    }
+
   }
 
   ngOnInit(): void {
