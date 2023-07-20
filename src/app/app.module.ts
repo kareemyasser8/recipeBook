04e35c1faad1089cd7163524d8a3d15ba81222bf
kareemyasser8 <kareem.yasserr@gmail.com>
2023-07-20 13:53:09 +0200

    creating auth feature module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,33 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthComponent } from './auth/auth.component';
+import { authModule } from './auth/auth.module';
+import { CoreModule } from './core.module';
 import { HeaderComponent } from './header/header.component';
 import { RecipesModule } from './recipes/recipes.module';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
-import { CoreModule } from './core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AuthComponent,
+
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
     SharedModule,
     RecipesModule,
     ShoppingListModule,
-    CoreModule
+    CoreModule,
+    authModule
   ],
   bootstrap: [AppComponent],
 })
