e1f755283cde3eddb2fcc2dc1f489647a90fa3e5
kareemyasser8 <kareem.yasserr@gmail.com>
2023-07-26 15:13:16 +0200

    creating shoppingList Reducer

---
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -0,0 +1,26 @@
+import { Ingredient } from '../../shared/ingredient.model';
+import * as shoppingListActions from './shopping-list.action';
+
+const initialState = {
+  ingredients: [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10)
+  ]
+};
+
+export function shoppingListReducer(
+        state = initialState,
+        action: shoppingListActions.AddIngredient){
+        switch (action.type){
+          case shoppingListActions.ADD_INGREDIENT:{
+            return {
+              ...state,
+              ingredients: [...state.ingredients, action.payload]
+            }
+          }
+          default: {
+            return state;
+          }
+        }
+
+      }
